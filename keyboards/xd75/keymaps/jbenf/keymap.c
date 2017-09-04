407942e02eda3987b78c4e52836bbdfdcaaa7ffb
Jeff Benfer <jbenfer@jbenfer.com>
2017-09-04 13:05:19 -0400

    caps lock now is lctrl when held

--- keyboards/xd75/keymaps/jbenf/keymap.c
+++ keyboards/xd75/keymaps/jbenf/keymap.c
@@ -38,16 +38,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | [      | UP     | ]      | N      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | CAPS   | LGUI   | LALT   | FN1    | SPACE  | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | SPACE  | FN2    | RALT   | RGUI   | RCTRL  |
+ * |CTL(CAP)| LGUI   | LALT   | FN1    | SPACE  | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | SPACE  | FN2    | RALT   | RGUI   | RCTRL  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
  [_QW] = { /* QWERTY */
-  { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_HOME, KC_GRV,  KC_PGUP, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC  },
-  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_END,  KC_BSLS, KC_PGDN, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL   },
-  { KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS, KC_QUOT, KC_EQL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT   },
-  { OSM(MOD_LSFT), KC_Z, KC_X, KC_C,    KC_V,    KC_B,    KC_LBRC, KC_UP,   KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RSFT)  },
-  { KC_CAPS, KC_LGUI, KC_LALT, TT(_LW), KC_SPC,  KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_SPC,  TT(_RS), KC_RALT, KC_RGUI, KC_RCTL  },
+  { KC_ESC,           KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_HOME, KC_GRV,  KC_PGUP, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC  },
+  { KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_END,  KC_BSLS, KC_PGDN, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL   },
+  { KC_LCTL,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS, KC_QUOT, KC_EQL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT   },
+  { OSM(MOD_LSFT),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_UP,   KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RSFT)  },
+  { CTL_T(KC_CAPS),   KC_LGUI, KC_LALT, TT(_LW), KC_SPC,  KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC,  KC_SPC,  TT(_RS), KC_RALT, KC_RGUI, KC_RCTL  },
  },
 
 /* LOWERED
